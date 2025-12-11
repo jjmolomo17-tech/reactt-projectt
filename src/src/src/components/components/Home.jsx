c7c223bf78ae53d17182395ae3ad686e98c7253e
Joy Molomo <jjmolomo17@gmail.com>
2025-12-12 00:36:51 +0200

    added a home and info

---
+++ src/src/src/components/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section id="home" className="h-screen flex flex-col justify-center items-center bg-gradient-to-r from-blue-500 to-purple-600 text-white">
+      <h1 className="text-5xl font-bold">Hi, I'm Joy</h1>
+      <p className="mt-4 text-xl">Software Developer & Designer</p>
+      <p className="mt-2">Building creative, responsive web experiences.</p>
+    </section>
+  );
+};
+
+export default Home;
+
